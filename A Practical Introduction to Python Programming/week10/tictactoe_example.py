37471d54c90244876c6df97ae6b6fa6e7ee127f6
Hoseop.kim <kimho314@gmail.com>
2026-09-18 20:39:38 +0200

    [feat]: add tictactoe example

--- A Practical Introduction to Python Programming/week10/tictactoe_example.py
+++ A Practical Introduction to Python Programming/week10/tictactoe_example.py
@@ -176,3 +176,23 @@ def WhoWon(game):
 #        [2, 1, 2]]
 
 # print(WhoWon(game))
+
+
+def DrawBoard(m):
+    if m <= 0: # raise an exception when zero or negative interger income
+        raise Exception("m must be a positive integer")
+    line1 = " ---" # keeps " ---" pattern 
+    line2 = "|   " # keeps "|   " pattern
+    row = ""
+    for i in range(m): # iterate m times
+        for j in range(m): # print " ---" m times 
+            row += line1
+        row += " \n"
+        for j in range(m): # print "|   " m times 
+            row += line2
+        row += "|\n"
+    for i in range(m): # print " ---" m times on the bottome of the board
+        row += line1
+    print(row)
+
+# DrawBoard(3)
